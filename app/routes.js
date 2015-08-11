a55f8aa7602c163924a1d1ab38c4a0a571a727dd
skatpgusskat <skatpgusskat@naver.com>
2015-08-12 01:15:56 +0900

    mid term commit

--- app/routes.js
+++ app/routes.js
@@ -3,50 +3,36 @@ var pg = require('pg');
 // app.routes.js
 module.exports = function(app, passport) {
 
-	app.get('/', function(req, res) {
-		var isLogin = req.user ? true : false;
-		res.render('pages/index',{
-			isLogin: isLogin
-		});
-	});
-
-	app.get('/db', function (req, res) {
-		console.log(process.env.DATABASE_URL);
-	 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		    client.query('SELECT * FROM test_table', function(err, result) {
-		      done();
-		      if (err)
-		       { console.error(err); res.send("Error " + err); }
-		      /*else
-		       { res.render('pages/db', {results: result.rows} ); }*/
-		    });
-	  	});
-	});
+    app.get('/', isLoggedIn, function(req, res) {
+        res.render('pages/index');
+    });
 
     app.get('/login', function(req, res) {
         // render the page and pass in any flash data if it exists
-        res.render('pages/login.ejs', { 
-        	message: req.flash('loginMessage'),
-        }); 
-    });
-
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('pages/profile.ejs', {
-            user : req.user // get the user out of session and pass to template
+        res.render('pages/login.ejs', {
+            message: req.flash('loginMessage'),
         });
     });
 
- // process the login form
+    // process the login form
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
+        successRedirect: '/', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
     }));
 
-    app.get('/logout', function(req,res){
-    	if(req.user)
-    		req.logout();
-    	res.redirect('/');
+    app.get('/logout', isLoggedIn, function(req, res) {
+        if (req.user)
+            req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/demo', function(req, res) {
+        res.render('pages/demo.ejs');
+    });
+
+    app.get('/student', isLoggedIn, function(req, res) {
+
     });
 };
 
@@ -58,5 +44,5 @@ function isLoggedIn(req, res, next) {
         return next();
 
     // if they aren't redirect them to the home page
-    res.redirect('/');
+    res.redirect('/login');
 }
